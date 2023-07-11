d8b5a9ba8312cbe229e24754cfecf8fd0ecff595
Thiago Honorato <thiagohbhonorato@gmail.com>
2023-07-11 16:23:28 -0300

    Fetch testing

--- src/app/api/generator/story/route.tsx
+++ src/app/api/generator/story/route.tsx
@@ -5,7 +5,7 @@ export async function GET() {
   const width = 1080;
   const height = 1920;
 
-  // await fetch("https://placehold.co/600x400/orange/white");
+  await fetch("https://placehold.co/600x400/orange/white");
 
   return new ImageResponse(
     (
@@ -22,12 +22,18 @@ export async function GET() {
       >
         <div
           style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             backgroundColor: "#fff",
             width: "100%",
             height: 600,
             borderRadius: 50,
           }}
-        ></div>
+        >
+          <h1 style={{ fontSize: 72 }}>Nome do produto</h1>
+          <h2 style={{ fontSize: 90, fontWeight: "bold" }}>R$ 100,00</h2>
+        </div>
       </div>
     ),
     {
